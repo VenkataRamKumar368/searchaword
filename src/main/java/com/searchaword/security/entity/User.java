dd0c7d01d3c6c21e26dbff380f3895ea32c66eac
Ram Kumar <ramkumarreddy3090@gmail.com>
2026-02-23 22:17:14 -0600

    Increase password column length to support BCrypt

--- src/main/java/com/searchaword/security/entity/User.java
+++ src/main/java/com/searchaword/security/entity/User.java
@@ -13,7 +13,7 @@ public class User {
     @Column(unique = true, nullable = false)
     private String username;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 255)
     private String password;
 
     @Column(nullable = false)
